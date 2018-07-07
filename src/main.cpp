594a14005109eb4a8f6895580d62fa0737d4e478
caiovictormc <caiovictor31@live.com>
2018-07-07 11:12:10 -0300

    connection and synchronization error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /*
 * @Author: ramonmelo
 * @Date:   2018-07-05
-* @Last Modified by:   ramonmelo
-* @Last Modified time: 2018-07-05
+* @Last Modified by:   caiovictormc
+* @Last Modified time: 2018-07-07 11:10:29
 */
 
 #define BLYNK_PRINT Serial
@@ -11,20 +11,65 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-char auth[] = "bb4604f08a224670805ef10c9e13438e";
 
-char ssid[] = "Venal Ninja Labs";
-char pass[] = "Venal@2016";
+// Auth Token in the Blynk App.
+char auth[] = "";
 
-void setup()
-{
-  // Debug console
+// WiFi credentials.
+char ssid[] = "";
+char pass[] = "";
+
+// Pins reserved for types
+const int amountTypes = 4;
+String pinTypes[amountTypes] = {"Arg 0", "Arg 1", "Arg 2", "Arg 3"};
+int bufferType = -1;
+
+
+void initialVirtualStatus() {
+  Blynk.virtualWrite(V10, "Selecione o");
+  Blynk.virtualWrite(V11, "tipo de produção");
+  for (int i=0; i <= amountTypes ; i++) {
+    Blynk.virtualWrite(i, 0);
+  }
+}
+
+
+void updateVirtualStatus(int pin) {
+  if (pin >= 0 || pin <= amountTypes) {
+    bufferType = pin;
+    Blynk.virtualWrite(V10, "Produção:");
+    Blynk.virtualWrite(V11, pinTypes[pin]);
+  }
+  for (int i=0; i <= amountTypes ; i++) {
+    if (i != pin) {
+      Blynk.virtualWrite(i, 0);
+    }
+  }
+}
+
+
+BLYNK_CONNECTED() {
+  if (bufferType != -1) {
+    updateVirtualStatus(bufferType);
+  } else {
+    initialVirtualStatus();
+  }
+
+}
+
+
+BLYNK_WRITE_DEFAULT() {
+  int pin = request.pin;
+  updateVirtualStatus(pin);
+}
+
+
+void setup() {
   Serial.begin(9600);
-
   Blynk.begin(auth, ssid, pass);
 }
 
-void loop()
-{
+
+void loop() {
   Blynk.run();
 }

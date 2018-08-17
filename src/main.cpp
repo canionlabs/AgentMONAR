b2b40648cdef232624fd1fd969e5f0905b0c7bab
Ramon Melo <me@ramonmelo.com>
2018-08-17 18:17:34 -0300

    Added: support to auto config using Blynk

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,21 @@
 * @Author: ramonmelo
 * @Date:   2018-07-05
 * @Last Modified by:   Ramon Melo
-* @Last Modified time: 2018-08-08
+* @Last Modified time: 2018-08-17
 */
 
+#define APP_DEBUG
 #define BLYNK_PRINT Serial // Comment out when deploy
 #define BLYNK_NO_BUILTIN
 #define BLYNK_NO_FLOAT
-
-#define DEVICE_NAME "Monar"
+#define USE_CUSTOM_BOARD
 
 #define UPDATE_RATE 60 // seconds
 #define ONE_WIRE_BUS D5
 
 #include "Arduino.h"
 #include <BlynkSimpleEsp8266.h>
+#include <BlynkProvisioning.h>
 #include <TimeLib.h>
 #include <WidgetRTC.h>
 
@@ -37,38 +38,35 @@ WidgetRTC rtc;
 
 std::vector<monar::Sensor*> sensors;
 
-char auth[] = "cb395bc6d9a742df9baeb14b3b41db1e";
-char address[] = "blynk.canionlabs.io";
-
 /// Main Scope
 
-void connect() {
-  WiFi.mode(WIFI_STA);
+// void connect() {
+//   WiFi.mode(WIFI_STA);
 
-  int cnt = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    BLYNK_LOG(".");
-    if (cnt++ >= 10) {
-      WiFi.beginSmartConfig();
-      while (1) {
-        delay(1000);
-        if (WiFi.smartConfigDone()) {
-          BLYNK_LOG("\nSmartConfig: Success");
-          break;
-        }
-        BLYNK_LOG("|");
-      }
-    }
-  }
+//   int cnt = 0;
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(500);
+//     BLYNK_LOG(".");
+//     if (cnt++ >= 10) {
+//       WiFi.beginSmartConfig();
+//       while (1) {
+//         delay(1000);
+//         if (WiFi.smartConfigDone()) {
+//           BLYNK_LOG("\nSmartConfig: Success");
+//           break;
+//         }
+//         BLYNK_LOG("|");
+//       }
+//     }
+//   }
 
-  BLYNK_LOG("WiFi Connected.");
-  WiFi.printDiag(Serial);
-  BLYNK_LOG("IP Address: %s\n", WiFi.localIP().toString().c_str());
-  BLYNK_LOG("Gateway IP: %s\n", WiFi.gatewayIP().toString().c_str());
-  BLYNK_LOG("Hostname: %s\n",   WiFi.hostname().c_str());
-  BLYNK_LOG("RSSI: %d dBm\n",   WiFi.RSSI());
-}
+//   BLYNK_LOG("WiFi Connected.");
+//   WiFi.printDiag(Serial);
+//   BLYNK_LOG("IP Address: %s\n", WiFi.localIP().toString().c_str());
+//   BLYNK_LOG("Gateway IP: %s\n", WiFi.gatewayIP().toString().c_str());
+//   BLYNK_LOG("Hostname: %s\n",   WiFi.hostname().c_str());
+//   BLYNK_LOG("RSSI: %d dBm\n",   WiFi.RSSI());
+// }
 
 void build_time(char *buffer) {
   sprintf(buffer, "%02d:%02d:%02d", hour(), minute(), second());
@@ -99,21 +97,20 @@ void service()
 }
 
 void setup() {
+  delay(500);
   Serial.begin(9600);
 
   sensors.push_back(new monar::SensorDallas(&oneWire));
   sensors.push_back(new monar::SensorInputVoltage());
 
-  connect();
-
-  Blynk.config(auth, address, 8080);
+  BlynkProvisioning.begin();
   timer.setInterval(1000L * UPDATE_RATE, service);
 
   BLYNK_LOG("done!");
 }
 
 void loop() {
-  Blynk.run();
+  BlynkProvisioning.run();
   timer.run();
 }
 
